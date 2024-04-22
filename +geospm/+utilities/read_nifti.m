e94ba20f0e7ffb7b359dd94b2cb76084c9790033
Highdimensional Neurology Group, UCL <>
2024-04-22 15:34:35 +0100

    Updated geospm.utilities.read_nifti() to read gzipped files.

--- +geospm/+utilities/read_nifti.m
+++ +geospm/+utilities/read_nifti.m
@@ -14,9 +14,20 @@
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
 function [data, data_type] = read_nifti(file_path)
-    
-    V = spm_vol(file_path);
+        
+    uncompressed_file_path = file_path;
+
+    if endsWith(file_path, '.gz')
+        uncompressed_file_path = gunzip(file_path);
+        uncompressed_file_path = uncompressed_file_path{1};
+    end
+
+    V = spm_vol(uncompressed_file_path);
     data_type = V.dt(1);
     
     data = spm_read_vols(V);
+
+    if ~strcmp(uncompressed_file_path, file_path)
+        hdng.utilities.delete(false, uncompressed_file_path);
+    end
 end
