84d6991d03db6d923582b7647c8e763b73e81886
Highdimensional Neurology Group, UCL <>
2024-06-03 23:15:33 +0100

    Removed backtransform_coordinates property from Subsampling and ensured minimal compatibility for volumetric indices. The only mode supported for now is 'identity'.

--- +geospm/+models/+sampling/Subsampling.m
+++ +geospm/+models/+sampling/Subsampling.m
@@ -30,7 +30,6 @@ classdef Subsampling < geospm.models.SamplingStrategy
         grid
         grid_options
         
-        backtransform_coordinates
         coincident_observations_mode
     end
     
@@ -49,7 +48,6 @@ classdef Subsampling < geospm.models.SamplingStrategy
             
             obj.grid_options = options.grid_options;
             obj.grid = grid;
-            obj.backtransform_coordinates = false;
             
             obj.coincident_observations_mode = geospm.models.sampling.Subsampling.JITTER_MODE;
         end
@@ -63,23 +61,10 @@ classdef Subsampling < geospm.models.SamplingStrategy
             
             attachments = spatial_data.attachments;
             
-            %{
-            [u, v, w] = obj.grid.space_to_grid(spatial_index.x, spatial_index.y, spatial_index.z);
-            
-            row_indices = obj.grid.clip_uvw(u, v, w);
+            [projected_index, row_indices] = spatial_index.project(obj.grid);
 
-            u = u(row_indices);
-            v = v(row_indices);
-            w = w(row_indices);
-            %}
-
-            [grid_spatial_index, row_indices, ~] = spatial_index.project(obj.grid);
-            u = grid_spatial_index.x_protected;
-            v = grid_spatial_index.y_protected;
-            w = grid_spatial_index.z_protected;
-            
             spatial_data = spatial_data.select(row_indices, []);
-            spatial_index = spatial_index.select_by_segment(row_indices);
+            %spatial_index = spatial_index.select_by_segment(row_indices);
 
             if ischar(N_samples)
                 if strcmp(N_samples, 'data')
@@ -92,27 +77,34 @@ classdef Subsampling < geospm.models.SamplingStrategy
             jitter_x = mod(rng.rand([N_samples 1]), 1);
             jitter_y = mod(rng.rand([N_samples 1]), 1);
             
+            sample_indices = randperm(rng, spatial_data.N, N_samples);
+            
+            %{
+            u = spatial_index.x;
+            v = spatial_index.y;
+            w = spatial_index.z;
+            
             %u = u - 1;
             %v = v - 1;
             w = w - 1;
             
-            sample_indices = randperm(rng, spatial_data.N, N_samples);
-            
             u = u(sample_indices, 1);
             v = v(sample_indices, 1);
             w = w(sample_indices, 1);
-            
+            %}
+
             switch obj.coincident_observations_mode
 
                 case geospm.models.sampling.Subsampling.IDENTITY_MODE
                     result = spatial_data.select(sample_indices, []);
-                    spatial_index = spatial_index.select_by_segment(sample_indices, @(specifier, modifier) obj.update_coordinates(specifier, modifier, u, v, w));
+                    spatial_index = projected_index.select_by_segment(sample_indices);
+                
+                %{
                 
                 case geospm.models.sampling.Subsampling.JITTER_MODE
                     result = spatial_data.select(sample_indices, []);
                     spatial_index = spatial_index.select_by_segment(sample_indices, @(specifier, modifier) obj.add_jitter(specifier, modifier, jitter_x, jitter_y, u, v, w));
                 
-                %{
                 case geospm.models.sampling.Subsampling.AVERAGE_MODE
                     result = spatial_data.select(sample_indices, [], @(arguments) obj.average_coincident_observations(arguments, u, v, w));
                 
@@ -135,27 +127,14 @@ classdef Subsampling < geospm.models.SamplingStrategy
     
     methods (Access=private)
         
-        function specifier = update_coordinates(obj, specifier, modifier, u, v, w) %#ok<INUSD>
-            
-            if ~obj.backtransform_coordinates
-                specifier.per_row.x = cast(u, 'double');
-                specifier.per_row.y = cast(v, 'double');
-                specifier.per_row.z = cast(w, 'double');
-            end
-        end
-
         function specifier = add_jitter(obj, specifier, modifier, jitter_x, jitter_y, u, v, w) %#ok<INUSD>
             
             u = cast(u, 'double') + jitter_x;
             v = cast(v, 'double') + jitter_y;
             
-            if obj.backtransform_coordinates
-                [specifier.per_row.x, specifier.per_row.y, specifier.per_row.z] = obj.grid.grid_to_space(u, v, w);
-            else
-                specifier.per_row.x = cast(u, 'double');
-                specifier.per_row.y = cast(v, 'double');
-                specifier.per_row.z = cast(w, 'double');
-            end
+            specifier.per_row.x = cast(u, 'double');
+            specifier.per_row.y = cast(v, 'double');
+            specifier.per_row.z = cast(w, 'double');
         end
         
         function result = average_coincident_observations(obj, args, u, v, w)
@@ -183,15 +162,9 @@ classdef Subsampling < geospm.models.SamplingStrategy
                         state, coordinates, indices, args.observations, constant_w), ...
                 coords, result);
             
-            if obj.backtransform_coordinates
-                result.x = args.x(result.row_map);
-                result.y = args.y(result.row_map);
-                result.z = args.z(result.row_map);
-            else
-                result.x = cast(u(result.row_map), 'double');
-                result.y = cast(v(result.row_map), 'double');
-                result.z = cast(w(result.row_map), 'double');
-            end
+            result.x = cast(u(result.row_map), 'double');
+            result.y = cast(v(result.row_map), 'double');
+            result.z = cast(w(result.row_map), 'double');
         end
         
         function result = pick_one_coincident_observation(obj, args, u, v, w)
@@ -219,15 +192,9 @@ classdef Subsampling < geospm.models.SamplingStrategy
                         state, coordinates, indices, args.observations, constant_w), ...
                 coords, result);
             
-            if obj.backtransform_coordinates
-                result.x = args.x(result.row_map);
-                result.y = args.y(result.row_map);
-                result.z = args.z(result.row_map);
-            else
-                result.x = cast(u(result.row_map), 'double');
-                result.y = cast(v(result.row_map), 'double');
-                result.z = cast(w(result.row_map), 'double');
-            end
+            result.x = cast(u(result.row_map), 'double');
+            result.y = cast(v(result.row_map), 'double');
+            result.z = cast(w(result.row_map), 'double');
         end
     end
     
